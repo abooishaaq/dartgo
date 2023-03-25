fdac8179d1e9c59ee8acbf98d4211b684f2046b5
Malik Ammar Faisal <binfaisal.ammar@gmail.com>
2023-03-25 10:52:45 +0530

    initial demo

---
+++ callee.go
@@ -0,0 +1,55 @@
+package main
+
+import "C"
+import (
+	"fmt"
+	"unsafe"
+)
+
+type Point struct {
+	X, Y float64
+}
+
+func NewPoint(x, y float64) Point {
+	return Point{x, y}
+}
+
+//export WrapNewPoint
+func WrapNewPoint(x, y float64) unsafe.Pointer {
+	mem := C.malloc(C.sizeof_double * 2)
+	p := (*Point)(mem)
+	*p = NewPoint(x, y)
+	return mem
+}
+
+// calling this from dart will cause a runtime error
+//
+//export OldNewPoint
+func OldNewPoint(x, y float64) unsafe.Pointer {
+	p := NewPoint(x, y)
+	return unsafe.Pointer(&p)
+}
+
+func AddPoint(p1, p2 Point) Point {
+	return Point{p1.X + p2.X, p1.Y + p2.Y}
+}
+
+//export WrapAddPoint
+func WrapAddPoint(p1, p2 unsafe.Pointer) unsafe.Pointer {
+	mem := C.malloc(C.sizeof_double * 2)
+	p := (*Point)(mem)
+	*p = AddPoint(*(*Point)(p1), *(*Point)(p2))
+	return mem
+}
+
+// calling this from dart will cause a runtime error
+//
+//export OldAddPoint
+func OldAddPoint(p1, p2 unsafe.Pointer) unsafe.Pointer {
+	p := AddPoint(*(*Point)(p1), *(*Point)(p2))
+	return unsafe.Pointer(&p)
+}
+
+func main() {
+	fmt.Println("main")
+}
---
+++ caller.dart
@@ -0,0 +1,56 @@
+import 'dart:ffi' as ffi;
+import 'dart:io' show Platform;
+
+// Define the Go struct
+class GoPoint extends ffi.Struct {
+  @ffi.Double()
+  external double x;
+
+  @ffi.Double()
+  external double y;
+}
+
+// Define the FFI functions
+typedef new_point_func = ffi.Pointer<GoPoint> Function(
+    ffi.Double x, ffi.Double y);
+typedef NewPoint = ffi.Pointer<GoPoint> Function(double x, double y);
+
+typedef add_point_func = ffi.Pointer<GoPoint> Function(
+    ffi.Pointer<GoPoint> p1, ffi.Pointer<GoPoint> p2);
+typedef AddPoint = ffi.Pointer<GoPoint> Function(
+    ffi.Pointer<GoPoint> p1, ffi.Pointer<GoPoint> p2);
+
+void main() {
+  // Load the shared library
+  var libraryPath = './callee.so';
+  if (Platform.isMacOS) {
+    libraryPath = './callee.dylib';
+  } else if (Platform.isWindows) {
+    libraryPath = 'callee.dll';
+  }
+  final dylib = ffi.DynamicLibrary.open(libraryPath);
+
+  // Look up the "NewPoint" function
+  final NewPoint newPoint = dylib
+      .lookup<ffi.NativeFunction<new_point_func>>('WrapNewPoint')
+      .asFunction();
+  final AddPoint addPoint = dylib
+      .lookup<ffi.NativeFunction<add_point_func>>('WrapAddPoint')
+      .asFunction();
+
+  // Call the "NewPoint" function and get the result as a Dart struct
+  final goPoint = newPoint(2.0, 3.0);
+  final goPoint2 = newPoint(4.0, 5.0);
+
+  // Print the result
+  print("point 1: ${goPoint.ref.x}, ${goPoint.ref.y}");
+  print("point 2: ${goPoint2.ref.x}, ${goPoint2.ref.y}");
+
+  final added = addPoint(goPoint, goPoint2);
+  print("added  : ${added.ref.x}, ${added.ref.y}");
+
+  final NewPoint NewPointFail = dylib
+      .lookup<ffi.NativeFunction<new_point_func>>('OldNewPoint')
+      .asFunction();
+  NewPointFail(2.0, 3.0);
+}
